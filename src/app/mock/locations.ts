3d11b034b02321baa61a8fada6987d6e7783a482
joeymalope-za <joey.malope@beamlend.com>
2024-04-22 13:51:19 +0200

    Init project scaffold

---
+++ src/app/mock/locations.ts
@@ -0,0 +1,11 @@
+export const locations: Array<string> = [
+  "Place 1",
+  "Place Placington 1",
+  "Place Placington 2",
+  "Not Place Placington",
+  "Place Avenue, pathway drive",
+  "Shouldn't show up Avenue, street road",
+  "Shogun Avenue, Tokyo road",
+  "Shanton Brollow, Tokyo road",
+  "Other place, anyon Drive",
+];
---
+++ src/app/type/index.ts
@@ -0,0 +1,10 @@
+export enum IconType {
+  home = "home",
+  newOrder = "newOrder",
+  quickCheck = "quickCheck",
+  appTracker = "appTracker",
+  myDocs = "myDocs",
+  myProfile = "myProfile",
+  more = "more",
+  calender = "calender",
+}
